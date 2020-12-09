07c84e6ad3bc3970b45f55bd0cafa8d8b41f2d51
Sean Campbell <sean.campbell13@gmail.com>
2020-12-09 18:45:32 -0500

    Initial

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import React from 'react'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
+import Box from '@material-ui/core/Box'
+import ProTip from '../src/ProTip'
+import Copyright from '../src/Copyright'
+import NextLink from 'next/link'
+import MuiLink from '@material-ui/core/Link'
 
 export default function Index() {
   return (
     <Container maxWidth="sm">
-      <Box my={4}>
+      <Box sx={{ my: 4 }}>
         <Typography variant="h4" component="h1" gutterBottom>
           Next.js v5-alpha with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
+        <NextLink href="/about" passHref>
+          <MuiLink color="secondary">Go to the about page</MuiLink>
+        </NextLink>
         <ProTip />
         <Copyright />
       </Box>
     </Container>
-  );
+  )
 }
